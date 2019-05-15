b19684ab4d326a3cc5a0e45a6be3130867fc898f
HongYang Gao <hongyang.gao@gmail.com>
2019-05-14 18:49:05 -0700

    Write toggleComplete method

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,12 @@ class App extends Component {
   }
 
   markComplete = (id) => {
-    console.log(id)
+    this.setState({todos: this.state.todos.map( todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    })})
   }
 
   render () {
